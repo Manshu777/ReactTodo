0971a082b3bb39d79d345c4db6ceeb4f4057432d
unknown <manshusmartboy@gmail.com>
2022-10-02 20:23:00 +0530

    Updates

---
+++ precache-manifest.b09cae9c43aa6f3ec554a24023a6e8fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b2164b422c42de31f7b",
+    "url": "/ReactTodo/static/css/main.56dcb62d.chunk.css"
+  },
+  {
+    "revision": "4b2164b422c42de31f7b",
+    "url": "/ReactTodo/static/js/main.4b2164b4.chunk.js"
+  },
+  {
+    "revision": "12a547d5be2359b88d04",
+    "url": "/ReactTodo/static/js/1.12a547d5.chunk.js"
+  },
+  {
+    "revision": "5deac06e4f762c64d1a9",
+    "url": "/ReactTodo/static/js/runtime~main.5deac06e.js"
+  },
+  {
+    "revision": "5d472cd5e9893632cadf852600688946",
+    "url": "/ReactTodo/index.html"
+  }
+];
